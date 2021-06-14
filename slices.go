9704a7ecae4ab8b6643087e73a5046924fef5b24
Gorkem Kockaya <gorkem.kockaya@gmail.com>
2021-06-14 07:25:49 +0300

    fix(slices): fix

--- slices.go
+++ slices.go
@@ -1,5 +1,7 @@
 package slices
 
+// import "reflect"
+
 func FindString(sl *[]string, i *string) (*string, bool) {
 	for _, c := range *sl {
 		if c == *i {
@@ -11,7 +13,7 @@ func FindString(sl *[]string, i *string) (*string, bool) {
 }
 
 // returns -1 if unable to find element
-func IndexOf(sl *[]interface{}, s *interface{}) (int, bool) {
+func IndexOfString(sl *[]string, s *string) (int, bool) {
 	var i int
 	for _, el := range *sl {
 		if el == *s {
@@ -22,3 +24,25 @@ func IndexOf(sl *[]interface{}, s *interface{}) (int, bool) {
 
 	return -1, false
 }
+
+// func takeSliceArg(arg interface{}) ([]interface{}, bool) {
+// 	slice, ok := takeArg(arg, reflect.Slice)
+// 	if !ok {
+// 		return nil, false
+// 	}
+
+// 	c := slice.Len()
+// 	out := make([]interface{}, c)
+
+// 	for i := 0; i < c; i++ {
+// 		out[i] = slice.Index(i).Interface()
+// 	}
+
+// 	return out, true
+// }
+
+// func takeArg(arg interface{}, kind reflect.Kind) (reflect.Value, bool) {
+// 	val := reflect.ValueOf(arg)
+
+// 	return val, val.Kind() == kind
+// }
